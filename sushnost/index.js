99f453eb17fd678437bb7cd693ac989f52c21928
SERGEYBOBKOV <sergey_bobkov@inbox.ru>
2018-03-25 18:47:40 +0300

    Возведение в квадрат стрелочная функция

--- sushnost/index.js
+++ sushnost/index.js
@@ -42,3 +42,19 @@ let numberTwo = [
 });
 console.log(numberTwo);
 //printMessage('msg', 'h1', numberTwo);
+
+console.log('========Возведение в квадрать декларативный способ стрелочная функция========');
+let numberTwoNext = [
+  0,
+  1,
+  2,
+  3,
+  4,
+  5,
+  6,
+  7,
+  8,
+  9
+].map(num => Math.pow(num,2));
+console.log(numberTwoNext);
+//printMessage('msg', 'h1', numberTwo);
